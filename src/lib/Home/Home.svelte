22e8facc54dc5cfe182ad0c0f0aca4fc806091bd
DamienSn <damienstephan07@gmail.com>
2021-12-22 13:12:54 +0100

    home section

---
+++ src/lib/Home/Home.svelte
@@ -0,0 +1,60 @@
+<script>
+	import MacButtons from './MacButtons.svelte';
+	import Typewriter from 'svelte-typewriter';
+</script>
+
+<section id="home">
+	<MacButtons />
+	<h1 class="home">Hello, I'm Damien</h1>
+	<Typewriter interval={60} delay={1000}><h2 class="home">> Young Web Developer</h2></Typewriter>
+	<!-- <h2 class="home terminal-cursor"><span>_</h2> -->
+
+	<div id="bg" ></div>
+</section>
+
+<style type="scss">
+	#home {
+		height: 100vh;
+	}
+
+	h1.home {
+		font-weight: normal;
+		font-size: 74px;
+	}
+
+	h2.home {
+		font-weight: normal;
+		font-family: 'IBM Plex Mono', monospace;
+	}
+
+	// Background
+	#bg {
+		position: absolute;
+		top: 50%;
+		height: 50vh;
+		width: 100%;
+		background: url('img/bg.png'), $onyx;
+		background-position: center;
+		background-size: cover;
+		background-blend-mode: lighten;
+	}
+
+	// Cut the background
+	#bg::before {
+		content: "";
+		position: absolute;
+		border-right: 50vw solid transparent;
+		border-left: 50vw solid $onyx;
+		border-top: 10vh solid $onyx;
+		border-bottom: 10vh solid transparent;
+	}
+	#bg::after {
+		content: "";
+		position: absolute;
+		bottom: 0;
+		border-right: 50vw solid $onyx;
+		border-left: 50vw solid transparent;
+		border-top: 10vh solid transparent;
+		border-bottom: 10vh solid $onyx;
+	}
+</style>
---
+++ src/lib/Home/MacButtons.svelte
@@ -0,0 +1,31 @@
+<div class="mac-buttons">
+    <div class="mac-button"></div>
+    <div class="mac-button"></div>
+    <div class="mac-button"></div>
+</div>
+
+<style type="scss">
+    .mac-buttons {
+        width: 65px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .mac-button {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        opacity: 0.45;
+    }
+
+    .mac-button:nth-child(1) {
+        background-color: #BB0A21;
+    }
+    .mac-button:nth-child(2) {
+        background-color: #FF9F1C;
+    }
+    .mac-button:nth-child(3) {
+        background-color: #00DA57;
+    }
+</style>
