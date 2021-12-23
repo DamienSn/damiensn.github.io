9ec01c8e4bf79d5d56d23c9bdf022605e5b6ebab
DamienSn <damienstephan07@gmail.com>
2021-12-23 15:47:23 +0100

    clean code

--- src/lib/skills/Skills.svelte
+++ src/lib/skills/Skills.svelte
@@ -13,51 +13,39 @@
 
 	<h2>Front-End</h2>
 	<div class="skills-bar">
-		<CardWrapper title="Basics">
-			<SkillCard title="HTML" icon="/img/logos/html.svg">The Hypertext Markup Language</SkillCard>
-			<SkillCard title="CSS" icon="/img/logos/css.svg">For styling HTML</SkillCard>
-			<SkillCard title="JavaScript" icon="/img/logos/js.svg">
-				A must to work in web development
-			</SkillCard>
-		</CardWrapper>
+		<SkillCard title="HTML" icon="/img/logos/html.svg">The Hypertext Markup Language</SkillCard>
+		<SkillCard title="CSS" icon="/img/logos/css.svg">For styling HTML</SkillCard>
+		<SkillCard title="JavaScript" icon="/img/logos/js.svg">
+			A must to work in web development
+		</SkillCard>
 
-		<CardWrapper title="Frameworks">
-			<SkillCard icon="/img/logos/react.svg" title="ReactJS">
-				ReactJS is a common choice for web development
-			</SkillCard>
-			<SkillCard icon="/img/logos/svelte.svg" title="Svelte">I love svelte</SkillCard>
-		</CardWrapper>
+		<SkillCard icon="/img/logos/react.svg" title="ReactJS">
+			ReactJS is a common choice for web development
+		</SkillCard>
+		<SkillCard icon="/img/logos/svelte.svg" title="Svelte">I love svelte</SkillCard>
 	</div>
 
 	<h2>Back-End</h2>
 	<div class="skills-bar">
-		<CardWrapper title="Languages">
-			<SkillCard icon="/img/logos/node.svg" title="NodeJS">The server side version of JS</SkillCard>
-		</CardWrapper>
+		<SkillCard icon="/img/logos/node.svg" title="NodeJS">The server side version of JS</SkillCard>
 
-		<CardWrapper title="Databases">
-			<SkillCard icon="/img/logos/mysql.svg" title="MySQL">
-				The most popular relationnal database
-			</SkillCard>
-		</CardWrapper>
+		<SkillCard icon="/img/logos/mysql.svg" title="MySQL">
+			The most popular relationnal database
+		</SkillCard>
 	</div>
 
 	<h2>Others</h2>
 	<div class="skills-bar">
-		<CardWrapper title="Languages">
-			<SkillCard icon="/img/logos/python.svg" title="Python">
-				Universal programming language
-			</SkillCard>
-		</CardWrapper>
+		<SkillCard icon="/img/logos/python.svg" title="Python">
+			Universal programming language
+		</SkillCard>
 
-		<CardWrapper title="Tools">
-			<SkillCard icon="/img/logos/docker.svg" title="Docker">
-				I can deploy container withh docker and docker-compose
-			</SkillCard>
-			<SkillCard icon="/img/logos/git.svg" title="Git">
-				Git is a must to work with other developers
-			</SkillCard>
-		</CardWrapper>
+		<SkillCard icon="/img/logos/docker.svg" title="Docker">
+			I can deploy container withh docker and docker-compose
+		</SkillCard>
+		<SkillCard icon="/img/logos/git.svg" title="Git">
+			Git is a must to work with other developers
+		</SkillCard>
 	</div>
 </section>
 
@@ -68,6 +56,8 @@
 
 	.skills-bar {
 		display: grid;
-		grid-template-columns: auto 1fr;
+		grid-template-columns: repeat(auto-fill, 250px);
+		grid-gap: 15px;
+		justify-content: center;
 	}
 </style>
