fc29ed98eb6bd56ffca560c32c50288f55f6c0ca
Damien Stéphan <damienstephan07@gmail.com>
2022-09-20 22:32:00 +0200

    add build

---
+++ build/_app/immutable/chunks/2-a410751c.js
@@ -0,0 +1,1 @@
+import{P as m}from"./_page-5a3c59cb.js";export{m as component};
---
+++ build/_app/immutable/components/pages/_page.svelte-f8bd7fb5.js
@@ -0,0 +1,1 @@
+import"../../chunks/index-654eed6b.js";import{P as a}from"../../chunks/_page-5a3c59cb.js";export{a as default};
